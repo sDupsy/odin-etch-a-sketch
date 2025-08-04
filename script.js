9b8e26f404f3df76c5c9546d74225545e0713dd7
sDupsy <123974978+sDupsy@users.noreply.github.com>
2025-08-04 13:14:16 +0200

    Add gridSizeButton functionality not finished

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const container = document.querySelector("#container");
+const button = document.querySelector(".sizeButton");
 
-const sixteenBySixteen = 16 * 16;
+let gridSize = 16 * 16;
 
-for (let i = sixteenBySixteen; i > 0; i--) {
-    let tile = document.createElement("div");
-    tile.setAttribute("class", `tile ${i}`)
-    container.appendChild(tile);
+function makeGrid (gridSize) {
+    for (let i = gridSize; i > 0; i--) {
+        let tile = document.createElement("div");
+        tile.setAttribute("class", `tile ${i}`)
+        container.appendChild(tile);
+
+    }
 }
 function colorBlack(elem) {
     const bgColor ="black";
     elem.style.backgroundColor = bgColor;
 }
 
-let nodes = document.querySelectorAll(".tile")
+let tiles = Array.from(document.querySelectorAll(".tile"));
 
-let tiles = Array.from(nodes);
+tiles.map(tile => tile.addEventListener("mouseover", () => (colorBlack(tile))));
 
-tiles.map(tile => tile.addEventListener("mouseover", () => (colorBlack(tile))))
 
-// for each tile
-// add mouseover eventlistener
-// if event color tile
+button.addEventListener("click", () => {
+    let size = prompt("max 100")
+    while (size > 100) {
+        size = prompt("max 100")
+    }
+    let newGridSize = size * size;
+    
+    makeGrid(newGridSize);
+});
+
+
+makeGrid(gridSize);
